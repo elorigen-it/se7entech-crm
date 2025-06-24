b08112e2ca6f6b513ff41106b275c8710440a0d5
elorigen-it <it@elorigen.restaurant>
2025-06-24 10:42:29 -0400

    changes to argon. avoid errors

--- js/argon.js
+++ js/argon.js
@@ -204,18 +204,18 @@ var FormControl = (function() {
 
 	// Methods
 
-	function init($this) {
-		$this.on('focus blur', function(e) {
-        $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
-    }).trigger('blur');
-	}
+	// function init($this) {
+	// 	$this.on('focus blur', function(e) {
+    //     $(this).parents('.form-group').toggleClass('focused', (e.type === 'focus' || this.value.length > 0));
+    // }).trigger('blur');
+	// }
 
 
 	// Events
 
-	if ($input.length) {
-		init($input);
-	}
+	// if ($input.length) {
+	// 	init($input);
+	// }
 
 })();
 
