d2e7cd9cecc12337758f8720d0f3cca4eae72bf0
elorigen-it <it@elorigen.restaurant>
2025-07-25 09:16:42 -0400

    Fix categories on empty

--- app/main.js
+++ app/main.js
@@ -3,11 +3,10 @@ import { Router } from './utils/router.js';
 // 1. Definir rutas y sus controladores
 const routes = {
   '/': () => import('./routes/home.js'),
+  '/modules/customer-portal/index.php/tasks': () => import('./routes/user-tasks.js'),
   '/modules/customers/index.php/:id/content-creator/generate': () => import('./routes/content-creator.js'),
   '/modules/customers/index.php/:id/brand-content/view/:id': () => import('./routes/view-brand-content.js'),
-  '/modules/customers/index.php/:id/brand-content/edit/:id': () => import('./routes/edit-brand-content.js'),
-  '/modules/tests/tasks': () => import('./routes/tasks.js'),
-  '/modules/customer-portal/index.php/tasks': () => import('./routes/user-tasks.js')
+  '/modules/customers/index.php/:id/brand-content/edit/:id': () => import('./routes/edit-brand-content.js')  
 };
 
 // 2. Inicializar router
